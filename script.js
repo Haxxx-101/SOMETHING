1a69276ef156137e22ce326707795d4cbfcebb9c
Haxxx-101 <mi9173505@gmail.com>
2025-05-12 21:53:41 +0700

    SOMETHING FOR HER

---
+++ script.js
@@ -0,0 +1,23 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () => {
+    question.innerHTML = "YESSSS, I LIKE U TOO!!!!";
+    gif.src = 
+      "https://i.ibb.co.com/8nTvCnGy/VID-20250507-181414-221-2-unscreen.gif";
+    });
+
+noBtn.addEventListener("mouseover", () => {
+    const noBtnRect = noBtn.getBoundingClientRect();
+    const maxX = window.innerWidth - noBtnRect.width;
+    const maxY = window.innerHeight - noBtnRect.height;
+
+    const randomX = Math.floor(Math.random() * maxX);
+    const randomY = Math.floor(Math.random() * maxY);
+
+    noBtn.style.left = randomX + "px";
+    noBtn.style.top = randomY + "px";
+});
